523bad07a3763d205d2e0b2838fd1ada53dbf819
as <as.utf8@gmail.com>
2017-09-13 14:51:39 -0700

    forgot pprof.go

---
+++ pprof.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func trypprof() func() {
+	if *cpuprofile == "" {
+		return func(){}
+	}
+	f, err := os.Create(*cpuprofile)
+	if err != nil {
+		log.Fatal(err)
+	}
+		pprof.StartCPUProfile(f)
+		return pprof.StopCPUProfile
+}
