23d174b99063879b26d9831ed2b55a1c6d060ce6
as <as.utf8@gmail.com>
2018-05-03 02:20:45 -0700

    v0.6.6

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/as/shiny/screen"
 	mus "github.com/as/text/mouse"
@@ -32,11 +33,20 @@ var (
 	focused   = false
 )
 
+func defaultFaceSize() int {
+	switch runtime.GOOS {
+	case "darwin":
+		return 13
+	default:
+		return 11
+	}
+}
+
 var (
 	utf8       = flag.Bool("u", false, "enable utf8 experiment")
 	elastic    = flag.Bool("elastic", false, "enable elastic tabstops")
 	oled       = flag.Bool("b", false, "OLED display mode (black)")
-	ftsize     = flag.Int("ftsize", 11, "font size")
+	ftsize     = flag.Int("ftsize", defaultFaceSize(), "font size")
 	srvaddr    = flag.String("l", "", "(dangerous) announce and serve file system clients on given endpoint")
 	clientaddr = flag.String("d", "", "dial to a remote file system on endpoint")
 	quiet      = flag.Bool("q", false, "dont interact with the graphical subsystem (use with -l)")
