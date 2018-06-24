bce6ccd9d973ed97b54d3bdde14640bff566d706
as <as.utf8@gmail.com>
2018-06-24 06:51:08 -0700

    add dump support to editor

---
+++ dump.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"io"
+	"os"
+
+	"github.com/as/a/dump"
+	"github.com/as/ui/col"
+	"github.com/as/ui/tag"
+)
+
+var ErrBadDump = errors.New("bad dump file")
+
+func Load(g *Grid, file string) (e error) {
+	defer func() {
+		dump.Printf("load: err: %v", e)
+	}()
+
+	fd, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	r := dump.NewScanner(fd)
+
+	var wd, f0, f1 string
+	if !r.Scan(&wd, &f0, &f1) {
+		return fmt.Errorf("load: wd: %v", r.Err())
+	}
+	if err = os.Chdir(wd); err != nil {
+		dump.Printf("cd %q: %s", wd, err)
+	}
+	f0, f1 = f0, f1
+
+	var colcents []int
+	if !r.Scan(&colcents) {
+		return fmt.Errorf("load: colcents: %v", r.Err())
+	}
+	dump.Printf("colcents: %#v\n", colcents)
+
+	glabel := ""
+	if !r.Scan('w', ' ', &glabel) {
+		return fmt.Errorf("load: glabel: %v", r.Err())
+	}
+	dump.Printf("grid: label: %q\n", glabel)
+
+	g.Tag.Win.Delete(0, g.Tag.Win.Len())
+	g.Tag.Win.InsertString(glabel, 0)
+	g.Tag.Win.Select(0, 0)
+
+	var cols []*col.Col
+	x := g.Loc().Min.X
+	dx := g.Loc().Dx()
+	for i, cent := range colcents {
+		var (
+			j      int
+			tlabel string
+		)
+		if !r.Scan('c', &j, &tlabel) {
+			return fmt.Errorf("load: tlabel: %v", r.Err())
+		}
+		if i != j {
+			dump.Printf("load: col: mismatch col ids: %d != %d", i, j)
+		}
+		dump.Printf("load: col: %d: tlabel: %q", i, tlabel)
+		c := col.New(g.Dev(), nil)
+		c.Tag.InsertString(tlabel, 0)
+		c.Tag.Select(0, 0)
+		col.Attach(g, c, image.Pt(x+cent*dx/100, 0))
+		cols = append(cols, c)
+	}
+
+Loop:
+	for {
+		var i, j, q0, q1, cent, nbody, wid, ntag, _, dir, dirty int
+		kind := byte('?')
+		if !r.Scan(&kind) {
+			break
+		}
+
+		switch kind {
+		case 'F':
+			r.Scan(&i, &j, &q0, &q1, &cent, &nbody, '\n', &wid, &ntag, &nbody, &dir, &dirty)
+		case 'f':
+			r.Scan(&i, &j, &q0, &q1, &cent, '\n', &wid, &ntag, &nbody, &dir, &dirty)
+			nbody = 0
+		default:
+			dump.Printf("load: win: c%dr%d: scan: kind %q: can't parse this type: ", i, j, kind)
+			break Loop
+		}
+
+		if r.Err() != nil {
+			dump.Printf("load: win: scan: %c: col(%d) row(%d): %v", kind, i, j, r.Err())
+			break
+		}
+
+		t := tag.New(g.Dev(), nil)
+		n, err := io.Copy(t.Win, io.LimitReader(r, int64(ntag)))
+		dump.Printf("tag: copy label: %d bytes: err (%v)", n, err)
+		t.Win.Select(0, 0)
+
+		if !r.Scan('\n') {
+			logf("tag label delim: %v", err)
+		}
+
+		n, err = io.Copy(t.Body.(io.Writer), io.LimitReader(r, int64(nbody)))
+		dump.Printf("tag: copy body: %d bytes: err (%v)", n, err)
+
+		t.Body.Select(int64(q0), int64(q1))
+		c := cols[i]
+		col.Attach(c, t, image.Pt(0, c.Loc().Min.Y+cent*c.Loc().Dy()/100))
+	}
+
+	return nil
+}
+
+func Dump(g *Grid, wdir string, font0, font1 string) {
+	d, _ := dump.Create("a.dump")
+	defer d.Close()
+
+	d.Line(wdir)
+	d.Line(font0)
+	d.Line(font1)
+
+	// Each column's % offset from the start of the X-axis
+	sep := byte(' ')
+	for i, c := range g.List {
+		if i+1 == len(g.List) {
+			sep = '\n'
+		}
+		d.Int(flattenX(c, g), sep)
+	}
+
+	// The label for the grid itself
+	fmt.Fprintf(d, "w %s\n", g.Tag.Win.Bytes())
+
+	// The label for each column along with the column's number
+	for i, c := range g.List {
+		c, _ := c.(*col.Col)
+		if c == nil {
+			continue
+		}
+		fmt.Fprintf(d, "c%11d %s\n", i, c.Tag.Win.Bytes())
+	}
+
+	wid := 1
+
+	// The windows
+	for i, c := range g.List {
+		c, _ := c.(*col.Col)
+		if c == nil {
+			continue
+		}
+
+		for j, t := range c.List {
+			t, _ := t.(*tag.Tag)
+			if c == nil {
+				continue
+			}
+			// x, e, f, F
+
+			q0, q1 := t.Body.Dot()
+			cent := flattenY(t, c)
+			ntag := t.Win.Len()
+			nbody := t.Body.Len()
+			dir := 0
+			dirty := 1
+
+			fmt.Fprintf(d, "F%11d %11d %11d %11d %11d %11d \n", i, j, q0, q1, cent, nbody)
+			fmt.Fprintf(d, "%11d %11d %11d %11d %11d %s\n%s", wid, ntag, nbody, dir, dirty, t.Win.Bytes(), t.Body.Bytes())
+			wid++
+		}
+	}
+}
+
+func flattenY(w, c Plane) int {
+	return 100 * (w.Loc().Min.Y - c.Loc().Min.Y) / c.Loc().Dy()
+}
+func flattenX(c, g Plane) int {
+	return 100 * (c.Loc().Min.X - g.Loc().Min.X) / g.Loc().Dx()
+}
